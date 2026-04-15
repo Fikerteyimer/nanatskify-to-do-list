73134ccb7b869f84db2e996afa9143baafada291
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-15 12:16:28 +0300

    created componenta and pages directory to organize our project

---
+++ my-todo/src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar(){
+    return(
+        <div className="w-full h-16 bg-yellow-100 flex items-center justify-center">
+            <h1 className="header">Taskify</h1>
+            <ul>
+                <li>Home</li>
+                <li>Features</li>
+                <li>Login</li>
+                <li>Sign up</li>
+            </ul>
+
+            
+
+        </div>
+
+    )
+
+}
+export default Navbar;
