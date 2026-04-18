c23b42bcb95078c3b87b1c7b07c70403c799600d
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-18 16:40:46 +0300

    Added better styling for the button component for color consistency and design issue

--- my-todo/src/Components/Button.jsx
+++ my-todo/src/Components/Button.jsx
@@ -1,7 +1,25 @@
-function Button({text}){
-    return(
-        <button className="bg-orange-400 mx-auto">{text}</button>
-    );
-
+function Button({ text }) {
+  return (
+    <button
+      className="
+        bg-blue-600 
+        text-white 
+        px-6 
+        py-2 
+        rounded-lg 
+        font-medium 
+        shadow-md 
+        hover:bg-blue-700 
+        hover:shadow-lg 
+        active:scale-95 
+        transition-all 
+        duration-200
+        mx-auto
+      "
+    >
+      {text}
+    </button>
+  );
 }
+
 export default Button;
