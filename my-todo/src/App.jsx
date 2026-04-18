713324134dc8db5f10981755c7eeec6f29de234c
Fikerte Yimer <fikerteyimer06gmail.com>
2026-04-18 16:02:19 +0300

    Added a routing to the app component

--- my-todo/src/App.jsx
+++ my-todo/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
-import Login from './Pages/Login'
-
-import SignUp from './Pages/SignUp'
+import {Route,Routes} from 'react-router-dom';
+import Home from './Pages/Home.jsx'
+import Login from './Pages/Login.jsx'
+import SignUp from './Pages/SignUp.jsx'
+import Features from './Pages/Features.jsx'
       
-
-
 function App() {
   return (
     <>
+    <Routes>
+      <Route index element={<Home/>}></Route>
+      <Route path="/features" element={<Features/>}></Route>
+      <Route path="/signUp" element={<SignUp/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+    </Routes>
    
  
-    <SignUp/>
-    <Login/>
+    
 
 
     </>
